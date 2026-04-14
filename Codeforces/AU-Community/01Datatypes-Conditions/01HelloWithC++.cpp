769b4814a4d64f7baadf8a4975c3473d1d0ddceb
Ibnul Misbah <abdullahibnulmisbah@gmail.com>
2026-04-14 19:50:25 +0600

    First Problem

---
+++ Codeforces/AU-Community/01Datatypes-Conditions/01HelloWithC++.cpp
@@ -0,0 +1,24 @@
+/*
+ * Bismillahir Rahmanir Rahim...
+ * Now I will solve a problem from Codeforces...
+ * Assuit University Community, 01 Sheet : Datatype - Conditions
+ *
+ * Problem : Say Hello With C++
+ * Writer  : Ibnul Misbah
+ * Source  : Codeforces
+ */ 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string name;
+    cin >> name;
+
+    cout << "Hello, " << name << endl;
+
+    return 0;
+}
