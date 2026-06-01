e098df80cd440eb955d5a151d4b158d95273f740
Ibnul Misbah <abdullahibnulmisbah@gmail.com>
2026-06-01 19:59:42 +0600

    Another Problem

---
+++ Codeforces/AU-Community/01Datatypes-Conditions/02basicDataTypes.cpp
@@ -0,0 +1,28 @@
+/*
+ * problem : Basic Data Type 
+ * writer  : Ibnul Misbah
+ * source  : Codeforces
+ */ 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int integer = 0; cin >> integer;
+
+    long long long_int = 0; cin >> long_int;
+    
+    char character = 'a'; cin >> character;
+
+    float floating = 0; cin >> floating;
+
+    double real_num = 0; cin >> real_num;
+    
+    cout << integer << '\n' << long_int << '\n' \
+    << character << '\n' << floating << '\n' \
+    << real_num << endl;
+
+    return 0;
+}
